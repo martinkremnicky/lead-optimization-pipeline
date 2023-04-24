fd5acbfc62ad957488f8d7591008acbf73a03fff
martinkremnicky <94135385+martinkremnicky@users.noreply.github.com>
2023-04-24 04:20:11 +0200

    added deletion mutation

--- pipeline/mutations.py
+++ pipeline/mutations.py
@@ -1,6 +1,7 @@
 import selfies as sf
 import random
 import functions as fn
+import numpy as np
 
 ALPHABET = sf.get_semantic_robust_alphabet() # Gets the alphabet of robust symbols
 
@@ -24,11 +25,28 @@ def fragment_insertion(selfies_molecule, fragment_size=3, random_size = True):
         - if ``random_size=True``, becomes upper size limit of fragment generated,
         else (if `=False`) becomes exact fragment size
     """
-    if random_size == True:
-        fragment_size = random.randint(1,fragment_size)
+    if random_size:
+        fragment_size = random.randint(0,fragment_size)
     selfies_molecule_list = list(sf.split_selfies(selfies_molecule))
     rnd_index = random.randint(0,len(selfies_molecule_list))
     rnd_sample = random.sample(list(ALPHABET), fragment_size) 
     rnd_sample = ''.join(rnd_sample)
     selfies_molecule_list.insert(rnd_index,rnd_sample)
+    return fn.validate(''.join(selfies_molecule_list))
+
+def deletion(selfies_molecule, n=1, random_size = True):
+    """
+    Delete `n` elements from a molecule
+    """
+
+    selfies_molecule_list = list(sf.split_selfies(selfies_molecule))
+
+    if random_size:
+        n = random.randint(0,n)
+
+    rnd_indexes = np.random.randint(0,len(selfies_molecule_list)-1, size=n).tolist()
+    if len(selfies_molecule_list)>n:
+        for i in rnd_indexes:
+            del selfies_molecule_list[i]
+
     return fn.validate(''.join(selfies_molecule_list))
